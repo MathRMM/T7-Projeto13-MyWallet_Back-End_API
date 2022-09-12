21cb0b2fbf245d4688a82832990f3f4ed0cfcfa2
Math <math@pop-os.localdomain>
2022-09-12 13:31:04 -0300

    teste git ignore

--- src/middleware/accounts.middleware.js
+++ src/middleware/accounts.middleware.js
@@ -5,7 +5,7 @@ export default async function accountMiddleware(req,res,next){
     const token = req.headers.authorization?.replace('Bearer ' , '')
     const {userId} = req.params
     if(!token || !userId) return res.sendStatus(401);
-
+    
     try {
        const session = await Mongo_Session({find: {token,}})
        console.log(session.userId.equals(ObjectId(userId)))
